7493a389831703df4be04e535a97fe812c6135cb
Kiczu <adrian.mamyto@gmail.com>
2025-11-11 11:38:34 +0100

    refactor(types): extract cart types to src/types/cart and remove from types/index

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,16 +7,3 @@ export type Product = {
   description?: string;
   variants?: { id: string; name: string }[];
 };
-
-export type CartProduct = {
-  id: number;
-  title: string;
-  price: number;
-  image?: string;
-};
-
-export type CartItem = {
-  product: CartProduct;
-  qty: number;
-  variantId?: string;
-};
