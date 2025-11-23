c8159813e3bb1e52e5a80edfcf65d9ace3f3439e
Kiczu <adrian.mamyto@gmail.com>
2025-11-23 19:14:48 +0100

    feat(navbar): integrate PATHS and add proper navigation links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,17 @@
 import {
   AppBar,
   Toolbar,
-  Typography,
   IconButton,
   Badge,
   Box,
   Button,
+  Link,
 } from "@mui/material";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useCart } from "@/providers/CartContext";
 import Image from "next/image";
+import { PATHS } from "@/config/paths";
 
 const Navbar = () => {
   const { totalQty, openCart } = useCart();
@@ -36,18 +37,49 @@ const Navbar = () => {
         >
           <MenuIcon />
         </IconButton>
-        <Image
-          src="/assets/logo/knk-logo-bord.png"
-          alt="logo"
-          width={110}
-          height={110}
-          priority
-        />
+        <Link href={PATHS.home}>
+          <Image
+            src="/assets/logo/knk-logo-bord.png"
+            alt="logo"
+            width={110}
+            height={110}
+            priority
+          />
+        </Link>
         <Box sx={{ flexGrow: 1 }} />
         <Box sx={{ display: { xs: "none", md: "flex" }, gap: 3 }}>
-          <Button color="inherit">Kolekcje</Button>
-          <Button color="inherit">O mnie</Button>
-          <Button color="inherit">Kontakt</Button>
+          <Button
+            component={Link}
+            href={PATHS.home}
+            variant="text"
+            color="inherit"
+          >
+            Strona główna
+          </Button>
+          <Button
+            component={Link}
+            href={PATHS.about}
+            variant="text"
+            color="inherit"
+          >
+            O mnie
+          </Button>
+          <Button
+            component={Link}
+            href={PATHS.shop}
+            variant="text"
+            color="inherit"
+          >
+            Sklep
+          </Button>
+          <Button
+            component={Link}
+            href={PATHS.contact}
+            variant="text"
+            color="inherit"
+          >
+            Kontakt
+          </Button>
         </Box>
         <IconButton color="inherit" aria-label="cart" onClick={openCart}>
           <Badge badgeContent={totalQty} color="primary">
