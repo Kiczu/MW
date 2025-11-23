2e79984635dc42cb03ad97307e66a1cdc8d007dc
Kiczu <adrian.mamyto@gmail.com>
2025-11-23 19:12:50 +0100

    chore: add remote image patterns for WP uploads

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        pathname: "/knk/wp-content/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "kobietanakole.pl",
+        pathname: "/knk/wp-content/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
